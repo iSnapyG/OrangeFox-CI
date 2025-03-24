9aefa896c07176028bb45f3d877ce4463117746e
Eddie Snapp <isnapyg@gmail.com>
2025-03-24 12:47:12 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,10 +2,10 @@
 
 # Device
 export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export DT_LINK="https://github.com/iSnapyG/android_device_samsung_gts9fewifi.git -b fox_9.0"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="gts9fewifi"
+export OEM="samsung"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
